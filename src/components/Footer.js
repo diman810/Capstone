71a11be3db2d77b61cc3f286c20162639d155ef0
NDL <diman810@ukr.net>
2023-11-21 22:48:11 +0200

    Added media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,13 @@
 import Nav from './Nav'
 import logo from "../assets/logo-second.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitterSquare,
+  faInstagramSquare,
+  faFacebookSquare,
+} from "@fortawesome/free-brands-svg-icons";
+
+
 function Footer(props) {
   return (
     <footer>
@@ -13,15 +21,22 @@ function Footer(props) {
         </section>
         <section className='contacts'>
           <h6>Contacts</h6>
-          <p>Adress</p>
-          <p>phone number</p>
-          <p>email</p>
+          <p>1020 West Madison Street</p>
+          <p>Chicago IL 60607</p>
+          <p>(312) 888-3041</p>
+          <p>nospam@nofill.com</p>
         </section>
         <section className='media'>
           <h6>Social Media Links</h6>
-          <p>Adress</p>
-          <p>phone number</p>
-          <p>email</p>
+          <a href='https://facebook.com/' target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faFacebookSquare} size="2x" /><p>Facebook</p>
+          </a>
+          <a href='https://twitter.com/' target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitterSquare} size="2x" /><p>Twitter</p>
+          </a>
+          <a href='https://instagram.com/' target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagramSquare} size="2x" /><p>Instagram</p>
+          </a>
         </section>
       </div>
     </footer>
